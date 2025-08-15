37b07ce7a59e97db14afa46ece4a86234829aeaa
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-15 07:02:19 -0500

    Add comments to clarify code functionality

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
 
+// Route to handle GET requests to the root URL
 app.get('/', (req, res) => {
   res.send('Willkommen beim Markerless AR Tag Generator!');
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server läuft unter http://localhost:${port}`);
 });
